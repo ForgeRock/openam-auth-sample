3d05c8038dbe2e61bc5b771da3482e1a164b8412
Quentin CASTEL <quentin.castel@forgerock.com>
2015-08-13 13:37:06 +0100

    Improved the Authentication custom module.
    Correct some code style and add some comments.
    
    The code style adopted before wasn't the one used in OpenAM.

--- src/main/java/org/forgerock/openam/examples/SampleAuthPrincipal.java
+++ src/main/java/org/forgerock/openam/examples/SampleAuthPrincipal.java
@@ -1,7 +1,7 @@
 /**
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2011-2013 ForgeRock AS. All Rights Reserved
+ * Copyright (c) 2011-2015 ForgeRock AS. All Rights Reserved
  *
  * The contents of this file are subject to the terms
  * of the Common Development and Distribution License
@@ -25,32 +25,25 @@
 
 package org.forgerock.openam.examples;
 
-
-
 import java.io.Serializable;
 import java.security.Principal;
 
-
-
-public class SampleAuthPrincipal implements Principal, Serializable
-{
-    private final String name;
-    private final static String CLASSNAME = "SampleAuthPrincipal";
+/**
+ * SampleAuthPrincipal represents the user entity.
+ */
+public class SampleAuthPrincipal implements Principal, Serializable {
     private final static String COLON = " : ";
 
+    private final String name;
 
+    public SampleAuthPrincipal(String name) {
 
-    public SampleAuthPrincipal(String name)
-    {
-        if (name == null)
-        {
+        if (name == null) {
             throw new NullPointerException("illegal null input");
         }
 
         this.name = name;
     }
-
-
 
     /**
      * Return the LDAP username for this <code> SampleAuthPrincipal </code>.
@@ -58,12 +51,9 @@ public class SampleAuthPrincipal implements Principal, Serializable
      * @return the LDAP username for this <code> SampleAuthPrincipal </code>
      */
     @Override
-    public String getName()
-    {
+    public String getName() {
         return name;
     }
-
-
 
     /**
      * Return a string representation of this <code> SampleAuthPrincipal </code>.
@@ -72,13 +62,9 @@ public class SampleAuthPrincipal implements Principal, Serializable
      *         <code>TestAuthModulePrincipal</code>.
      */
     @Override
-    public String toString()
-    {
-        return new StringBuilder().append(CLASSNAME).append(COLON)
-                .append(name).toString();
+    public String toString() {
+        return new StringBuilder().append(this.getClass().getName()).append(COLON).append(name).toString();
     }
-
-
 
     /**
      * Compares the specified Object with this <code> SampleAuthPrincipal </code>
@@ -92,32 +78,25 @@ public class SampleAuthPrincipal implements Principal, Serializable
      *         <code> SampleAuthPrincipal </code>.
      */
     @Override
-    public boolean equals(Object o)
-    {
-        if (o == null)
-        {
+    public boolean equals(Object o) {
+        if (o == null) {
             return false;
         }
 
-        if (this == o)
-        {
+        if (this == o) {
             return true;
         }
 
-        if (!(o instanceof SampleAuthPrincipal))
-        {
+        if (!(o instanceof SampleAuthPrincipal)) {
             return false;
         }
         SampleAuthPrincipal that = (SampleAuthPrincipal) o;
 
-        if (this.getName().equals(that.getName()))
-        {
+        if (this.getName().equals(that.getName())) {
             return true;
         }
         return false;
     }
-
-
 
     /**
      * Return a hash code for this <code> SampleAuthPrincipal </code>.
@@ -125,8 +104,7 @@ public class SampleAuthPrincipal implements Principal, Serializable
      * @return a hash code for this <code> SampleAuthPrincipal </code>.
      */
     @Override
-    public int hashCode()
-    {
+    public int hashCode() {
         return name.hashCode();
     }
 }
